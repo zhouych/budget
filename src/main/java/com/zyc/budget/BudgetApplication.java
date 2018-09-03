8dc7e7ea4151817832e03c2e93a2f4e5bcafd3f0
zhouych <zhouych329@163.com>
2018-09-03 10:18:10 +0800

    ...

--- src/main/java/com/zyc/budget/BudgetApplication.java
+++ src/main/java/com/zyc/budget/BudgetApplication.java
@@ -1,7 +1,9 @@
 package com.zyc.budget;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +13,13 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class BudgetApplication {
 
-	@Bean("restTemplate")
+	@Autowired
+	private RestTemplateBuilder restTemplateBuilder;
+
+	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		return restTemplateBuilder.build();
 	}
 	
 	public static void main(String[] args) {
